5be905e296b57cb99a542a28e56ec96529b2d0a4
jeanettepranin <jeanettepranin@gmail.com>
2015-12-19 00:54:46 -0800

    delete senstitive info

--- server.js
+++ server.js
@@ -1,30 +1,7 @@
 var express=require('express');
-// var nodemailer = require("nodemailer");
 var app=express();
-/*
-Here we are configuring our SMTP Server details.
-STMP is mail server which is responsible for sending and recieving email.
-// */
-// var smtpTransport = nodemailer.createTransport("SMTP",{
-// 	service: "Gmail",
-// 	auth: {
-// 	user: "jeanettepranin@gmail.com",
-// 	pass: "peacelove2"
-// 	}
-// });
 var sendgrid  = require('sendgrid')(process.env.SENDGRID_USER, process.env.SENDGRID_PASS);
-// var sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
-// var email     = new sendgrid.Email({
-//   to:       'jeanettepranin@gmail.com',
-//   from:     'you@yourself.com',
-//   subject:  'Subject goes here',
-//   text:     'Hello world'
-// });
-// sendgrid.send(email, function(err, json) {
-//   if (err) { return console.error(err); }
-//   console.log(json);
-// });
-// make express look in the public directory for assets (css/js/img)
+
 app.use(express.static(__dirname + '/public'));
 /*------------------SMTP Over-----------------------------*/
 
@@ -52,14 +29,6 @@ app.get('/send',function(req,res){
 		}
 	});
 });
-
-/*--------------------Routing Over----------------------------*/
-// app.use(express.static(__dirname + '/css'));
-// app.use(express.static(__dirname + '/font-awesome'));
-// app.use(express.static(__dirname + '/fonts'));
-// app.use(express.static(__dirname + '/img'));
-// app.use(express.static(__dirname + '/js'));
-// app.use(express.static(__dirname + '/less'));
 
 
 var port = process.env.PORT || 3000;
